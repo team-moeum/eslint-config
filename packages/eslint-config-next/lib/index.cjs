ebf6647a5122f89f3eefe0f7cf2781ee4ce881d2
seungdeok <akk9132@naver.com>
2024-12-17 23:00:43 +0900

    chore: add next package

--- packages/eslint-config-next/lib/index.cjs
+++ packages/eslint-config-next/lib/index.cjs
@@ -1,6 +1,6 @@
 export default {
   env: { browser: true, es2021: true },
-  extends: ["next/core-web-vitals", "@moeum/eslint-react"],
+  extends: ["next/core-web-vitals", "@moeum/eslint-config-react"],
   settings: {
     react: {
       version: "detect",
